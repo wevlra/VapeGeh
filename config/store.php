55d1c5b4c98c8b1da020246e77180805db4ad7fb
ItsAzni <aznidev26@gmail.com>
2026-06-27 22:40:50 +0700

    feat: add store config and shared header partial

---
+++ config/store.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'name' => 'Vape Geh',
+    'address' => 'Perum Taman Niaga, Jl. Tanjakan, Cimuning, Kec. Mustika Jaya, Kota Bks, Jawa Barat 17158',
+    'phone' => '0812-3456-7890',
+];
