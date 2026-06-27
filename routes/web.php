2c74009611be52e4108ff5535e2bd62d82af7eb2
ItsAzni <aznidev26@gmail.com>
2026-06-27 22:45:11 +0700

    feat: add print receipt action and route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,14 @@
 <?php
 
+use App\Models\StockMovement;
 use Illuminate\Support\Facades\Route;
 
 Route::livewire('/', 'landing');
+
+Route::middleware(['web', 'auth'])->group(function () {
+    Route::get('/admin/stock-movements/{stockMovement}/receipt', function (StockMovement $stockMovement) {
+        $stockMovement->load(['product', 'location', 'creator', 'buyer', 'related']);
+
+        return view('receipts.receipt', ['movement' => $stockMovement]);
+    })->name('admin.stock-movements.receipt');
+});
